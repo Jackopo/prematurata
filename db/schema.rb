5f2cab392a922c0876cfb9cf21ea7826ee595673
Jacopo Ghisolfi <jacopo.ghisolfi@infopark.de>
2016-01-18 21:53:00 +0100

    Created Pages resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115164929) do
+ActiveRecord::Schema.define(version: 20160115171607) do
 
   create_table "casein_admin_users", force: :cascade do |t|
     t.string   "login",                           null: false
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20160115164929) do
     t.string   "time_zone"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
